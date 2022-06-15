e918d4fe1be83c1d05b0df4a113ee77ef803e6ab
chakravarthimattaparthi <89298554+chakravarthimattaparthi@users.noreply.github.com>
2022-06-15 22:52:58 +0530

    leaf fall animation

--- src/app/common-components/home/home.component.ts
+++ src/app/common-components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TweenMax , TweenLite, Linear, Sine} from 'gsap';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,6 +12,7 @@ export class HomeComponent implements OnInit {
   bottomTitle:string = '';
   loadCount: number = 0;
   loading: boolean = false;
+  falling = true;
 
   titles = [{
     topTile: 'Make Your Kids',
@@ -32,15 +33,28 @@ export class HomeComponent implements OnInit {
     this.topTitle = this.titles[0].topTile;
     this.bottomTitle = this.titles[0].bottomTitle;
     this.loading = true;
-    this.renderSliders();
-    const NUMBER_OF_LEAVES = 30;   
-    var container = document.getElementById('leafContainer') as HTMLVideoElement ;   
-    /* Fill the empty container with new leaves */  
-    for (var i = 0; i < NUMBER_OF_LEAVES; i++)    
-    {   
-        container.appendChild(this.createALeaf());   
-    }   
+    this.renderSliders(); 
+
+
+    TweenLite.set("#container",{perspective:600})
+    TweenLite.set("img",{xPercent:"-50%",yPercent:"-50%"})
+
+    var total = 30;
+    const container = document.getElementById("container") as HTMLElement,	w = window.innerWidth , h = window.innerHeight;
+ 
+    for (var i=0; i<total; i++){ 
+      var Div = document.createElement('div');
+      TweenLite.set(Div,{attr:{class:'dot'},x:this.R(0,w),y:this.R(-200,-150),z:this.R(-200,200)});
+      container.appendChild(Div);
+      this.animm(Div);
+    }
   }
+
+  animm(elm: any){   
+    TweenMax.to(elm,this.R(6,15),{y:window.innerHeight+100,ease:Linear.easeNone,repeat:-1,delay:-15});
+    //TweenMax.to(elm,this.R(4,8),{x:'+=100',rotationZ:this.R(0,180),repeat:-1,yoyo:true,ease:Sine.easeInOut});
+    //TweenMax.to(elm,this.R(2,8),{rotationX:this.R(0,360),rotationY:this.R(0,360),repeat:-1,yoyo:true,ease:Sine.easeInOut,delay:-5});
+  };
 
   renderSliders() {
 
@@ -67,91 +81,6 @@ export class HomeComponent implements OnInit {
       this.renderSliders();
     },7000); 
 
-  }
-
-
-  /*  
-    Receives the lowest and highest values of a range and  
-    returns a random integer that falls within that range.  
-*/  
-randomInteger(low: any, high: any)   
-{   
-    let test = high - low;
-    return low + Math.floor(Math.random() * (test));   
-}   
-  
-/*  
-   Receives the lowest and highest values of a range and  
-   returns a random float that falls within that range.  
-*/  
-randomFloat(low: any, high: any)   
-{   
-  let test1 = high - low;
-    return low + Math.random() * (test1);   
-}   
-  
-/*  
-    Receives a number and returns its CSS pixel value.  
-*/  
-pixelValue(value: any)   
-{   
-    return value + 'px';   
-}   
-  
-/*  
-    Returns a duration value for the falling animation.  
-*/  
-  
- durationValue(value: any)   
-{   
-    return value + 's';   
-}   
-  
-/*  
-    Uses an img element to create each leaf. “Leaves.css” implements two spin   
-    animations for the leaves: clockwiseSpin and counterclockwiseSpinAndFlip. This  
-    function determines which of these spin animations should be applied to each leaf.  
- 
-*/  
-createALeaf()   
-{   
-    /* Start by creating a wrapper div, and an empty img element */  
-    var leafDiv = document.createElement('div');   
-    var image = document.createElement('img');   
-  
-    /* Randomly choose a leaf image and assign it to the newly created element */  
-    image.src = '../../../assets/icons/icon' + this.randomInteger(1, 5) + '.png';  
-    image.width = 20;
-  
-    leafDiv.style.top = '-100px';   
-  
-    /* Position the leaf at a random location along the screen */  
-    leafDiv.style.left = this.pixelValue(this.randomInteger(0, 500));   
-  
-    /* Randomly choose a spin animation */  
-    var spinAnimationName = (Math.random() < 0.5) ? 'clockwiseSpin' : 'counterclockwiseSpinAndFlip';   
-  
-    /* Set the -webkit-animation-name property with these values */  
-    leafDiv.style.animationName = 'fade, drop';   
-    image.style.animationName = spinAnimationName;   
-  
-    /* Figure out a random duration for the fade and drop animations */  
-    var fadeAndDropDuration = this.durationValue(this.randomFloat(5, 11));   
-  
-    /* Figure out another random duration for the spin animation */  
-    var spinDuration = this.durationValue(this.randomFloat(4, 8));   
-    /* Set the -webkit-animation-duration property with these values */  
-    leafDiv.style.animationDuration = fadeAndDropDuration + ', ' + fadeAndDropDuration;   
-  
-    var leafDelay = this.durationValue(this.randomFloat(0, 5));   
-    leafDiv.style.animationDelay = leafDelay + ', ' + leafDelay;   
-  
-    image.style.animationDuration = spinDuration;   
-  
-    // add the <img> to the <div>   
-    leafDiv.appendChild(image);   
-  
-    /* Return this img element so it can be added to the document */  
-    return leafDiv;   
-}   
+  } 
+  R(min:any,max: any) {return min+Math.random()*(max-min)};
 }
